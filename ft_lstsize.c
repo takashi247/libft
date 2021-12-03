414308011c4ebf199d34b0a76c4299650bde177f
takashi247 <takashi247@gmail.com>
2021-12-03 18:56:24 +0900

    revise lstsize to use size_t

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnishina <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tnishina <tnishina@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 16:43:28 by tnishina          #+#    #+#             */
-/*   Updated: 2021/09/11 16:43:28 by tnishina         ###   ########.fr       */
+/*   Updated: 2021/12/03 18:56:11 by tnishina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int
+size_t
 	ft_lstsize(t_list *lst)
 {
-	int	len_lst;
+	size_t	len_lst;
 
 	if (!lst)
 		return (0);
